b1e165f75a708b7952f709b1ea7ae33b1178092a
syy <910427747@qq.com>
2023-08-17 14:30:03 +0800

    feat：文案修改

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -381,17 +381,15 @@ export const ChatInput = ({
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
-          href="https://github.com/mckaywrigley/chatbot-ui"
+          href="https://www.shanpersonage.com.cn/"
           target="_blank"
           rel="noreferrer"
           className="underline"
         >
-          ChatBot UI
+          个人博客
         </a>
-        .{' '}
-        {t(
-          "Chatbot UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
-        )}
+        &nbsp;
+        海底月是天上月，眼前人是心上人。向来心是看客心，奈何人是剧中人。
       </div>
     </div>
   );
