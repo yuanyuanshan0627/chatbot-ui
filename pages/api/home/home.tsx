b4d2049e00474a23218363829497fa0fc06309d9
syy <910427747@qq.com>
2023-08-17 17:20:41 +0800

    feat：修改标题图标

--- pages/api/home/home.tsx
+++ pages/api/home/home.tsx
@@ -366,7 +366,7 @@ const Home = ({
           name="viewport"
           content="height=device-height ,width=device-width, initial-scale=1, user-scalable=no"
         />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo.png" />
       </Head>
       {selectedConversation && (
         <main
